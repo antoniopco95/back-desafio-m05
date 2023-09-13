28865758072769dfda0934bb9cb1717c0bb8b2b2
Matheus Lucas Ferreira <Matheuslucasepg@gmail.com>
2023-09-13 20:44:52 -0300

    Update userValidator.js

--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -4,10 +4,6 @@ validateEmailDomain = (email) => {
     const regex =/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return regex.test(email);
 };
-async function emailExists(email) {
-    const result = await knex('usuarios').where('email', email).first();
-    return !!result;
-}
 module.exports={
-    validateEmailDomain, emailExists
+    validateEmailDomain
 }
