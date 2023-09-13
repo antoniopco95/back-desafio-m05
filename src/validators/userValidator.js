a7ee74c5c321620d259148bf165b54b307ecff57
Matheus Lucas Ferreira <Matheuslucasepg@gmail.com>
2023-09-13 16:50:49 -0300

    Update userValidator.js

--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -1,4 +1,8 @@
-exports.validateEmailDomain = (email) => {
+validateEmailDomain = (email) => {
     const regex =/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return regex.test(email);
 };
+
+module.exports={
+    validateEmailDomain
+}
