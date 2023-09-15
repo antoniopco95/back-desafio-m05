416761b5f0452c97fdc0a08f77279ccf91712c8d
Matheus Lucas Ferreira <Matheuslucasepg@gmail.com>
2023-09-15 17:31:41 -0300

    charges commit

---
+++ src/controllers/charges.js
@@ -0,0 +1,56 @@
+const knex = require("knex")(require("../knexfile").development);
+
+const chargesOverdue = async (req,res)=> {
+
+try {
+    const overdue = await knex.select('cobranca.*', 'cliente.nome')
+                                            .from('cobranca')
+                                            .join('cliente', 'cobranca.cliente_id', 'cliente.cliente_id')
+                                            .where('cobranca.status', 'Vencida');
+
+    let totalDue = 0 
+    overdue.forEach(element => {
+        totalDue += parseFloat(element.valor)
+    })
+
+    return res.status(200).json({cobrancas_vencidas:overdue, Total_Vencido:totalDue});
+} catch (error) {
+     res.status(500).send('Erro ao buscar o total devido por cliente.');
+}
+
+}
+const expectedCharges = async (req,res) => {
+    try {
+    const expected = await await knex.select('cobranca.*', 'cliente.nome')
+                                            .from('cobranca')
+                                            .join('cliente', 'cobranca.cliente_id', 'cliente.cliente_id')
+                                            .where('cobranca.status', 'Prevista');
+    let totalExpected = 0 
+    expected.forEach(element => {
+        totalExpected += parseFloat(element.valor)
+    })
+                                            
+    res.json({cobrancas_previstas:expected, total_previsto:totalExpected});
+} catch (error) {
+     res.status(500).send('Erro ao buscar o total devido por cliente.');
+}
+}
+const paidCharges = async (req,res)=>{
+        try {
+    const paid = await knex.select('cobranca.*', 'cliente.nome')
+                                            .from('cobranca')
+                                            .join('cliente', 'cobranca.cliente_id', 'cliente.cliente_id')
+                                            .where('cobranca.status', 'Paga');
+    let totalpaid = 0 
+    paid.forEach(element => {
+        totalpaid += parseFloat(element.valor)
+    })
+
+    res.json({cobrancas_pagas:paid, total_pago:totalpaid});
+} catch (error) {
+    console.log(error)
+     res.status(500).send('Erro ao buscar o total devido por cliente.');
+}
+}
+
+module.exports={chargesOverdue, expectedCharges, paidCharges}
---
+++ src/controllers/client.js
@@ -0,0 +1,17 @@
+const knex = require("knex")(require("../knexfile").development);
+
+knex
+const getClient = async (req,res)=>{
+    try{
+        const client = await knex('cliente').select("*");
+        res.status(200).json(client)
+
+    }catch(error){
+        console.log(error)
+        res.status(500).send('Erro ao buscar clientes.');
+    }
+}
+const createNewClient= async (req,res)=>{
+    
+}
+module.exports={getClient}
