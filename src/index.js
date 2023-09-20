b148f8d95c1032a08f1c7fd0ff2e6f8466df5f63
antoniopco95 <antoniopco95@gmail.com>
2023-09-19 22:06:34 -0300

    ajuste cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const rotas = require("./routes");
 const PORT = process.env.PORT || 8000;
-
+const cors = require("cors");
 const app = express();
-
 
 app.use(express.json());
 app.use(rotas);
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server on na rota ${PORT}`);
