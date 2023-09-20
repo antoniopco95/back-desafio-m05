e0e1f44e41e9b1fe25acc39fd271488538654f8c
antoniopco95 <antoniopco95@gmail.com>
2023-09-19 22:12:05 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ const PORT = process.env.PORT || 8000;
 const cors = require("cors");
 const app = express();
 
+const corsOptions = {
+  origin: "https://front-equipe-09-git-main-antoniopco95.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 204, // No Content response for preflight requests
+  allowedHeaders: "Content-Type,Authorization",
+};
+
 app.use(express.json());
 app.use(rotas);
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
   console.log(`Server on na rota ${PORT}`);
