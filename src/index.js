4d50f3f1f41667febaa59974f34e506d0b8c009d
antoniopco95 <antoniopco95@gmail.com>
2023-09-20 15:49:11 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const rotas = require("./routes");
 const PORT = process.env.PORT || 8000;
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
 app.use(
   cors({
@@ -11,13 +11,13 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.use(
+/* app.use(
   cors({
     origin: "https://front-equipe-09-git-main-antoniopco95.vercel.app",
     methods: ["GET", "POST", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
-);
+); */
 app.use(express.json());
 app.use(rotas);
 
