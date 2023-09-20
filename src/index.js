7a447792efb3739e52c365c2fb37592a2237a6e8
Matheus Lucas Ferreira <Matheuslucasepg@gmail.com>
2023-09-20 09:24:51 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ const PORT = process.env.PORT || 8000;
 const cors = require("cors");
 const app = express();
 
-const corsOptions = {
-  origin: "https://front-equipe-09-git-main-antoniopco95.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-};
-
+app.use(
+  cors({
+    origin: "https://front-equipe-09-git-main-antoniopco95.vercel.app",
+    methods: ["GET", "POST", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }))
 app.use(express.json());
 app.use(rotas);
-app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
   console.log(`Server on na rota ${PORT}`);
