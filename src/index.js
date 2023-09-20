8b9462c95422eebd3dfa9fb93516cb91ffc2ff20
Matheus Lucas Ferreira <Matheuslucasepg@gmail.com>
2023-09-20 15:37:42 -0300

    aaaa

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 const express = require("express");
 const rotas = require("./routes");
 const PORT = process.env.PORT || 8000;
-const cors = require("cors");
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://front-equipe-09-git-main-antoniopco95.vercel.app",
-    methods: ["GET", "POST", "PUT"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
 app.use(express.json());
 app.use(rotas);
 
